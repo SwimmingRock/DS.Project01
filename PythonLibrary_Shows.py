76406a5b3bac70df4c707d13adf5e5609fefe162
Rahul Patkar <122588925+SwimmingRock@users.noreply.github.com>
2023-07-02 15:46:19 -0700

    Function_Create Directories and Subdirectories
    A Function to create directories and subdirectories was added

--- PythonLibrary_Shows.py
+++ PythonLibrary_Shows.py
@@ -17,3 +17,15 @@ def create_directory(directory_path: str, directory_name: str) -> bool:
         os.makedirs(directory)
         print(f"Directory '{directory}' created successfully!")
 
+def create_subdirectories(directory_path: str, directory_name: str, subdirectories: List[str]) -> bool:
+    # Create the main directory
+    main_directory_path = os.path.join(directory_path, directory_name)
+    create_directory(directory_path, directory_name)
+
+# READ A LIST OF ALL CREATED SHOWS
+    # Create subdirectories within the main directory
+    for subdirectory in subdirectories:
+        subdirectory_path = os.path.join(main_directory_path, subdirectory)
+        create_directory(main_directory_path, subdirectory)
+
+# END: ___________CREATE NEW SHOWS/READ A LIST OF ALL CREATED SHOWS___________
